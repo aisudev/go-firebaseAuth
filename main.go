64bcb1ef890aaac7ef5131ae502a40d7ad4aedad
aisudev <tk.thanatorn@gmail.com>
2021-07-27 23:36:58 +0700

    done

--- main.go
+++ main.go
@@ -13,16 +13,10 @@ func main() {
 		return c.String(http.StatusOK, "Hi")
 	})
 
-<<<<<<< HEAD
 	e.Start(":8080")
 	// if err := e.StartAutoTLS(":9999"); err != nil {
 	// 	log.Fatalln(err.Error())
 	// }
-=======
-	if err := e.StartAutoTLS(":8080"); err != nil {
-		log.Fatalln(err.Error())
-	}
->>>>>>> 31f98fd151de4c5ee91d31de001a5c5e5c09f999
 	// if err := e.StartTLS(":9999", "server.cert", "server.key"); err != nil {
 	// 	log.Fatalln(err.Error())
 	// }
